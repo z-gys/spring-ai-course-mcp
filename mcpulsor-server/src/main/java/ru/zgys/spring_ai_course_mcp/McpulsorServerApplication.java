3164fd580071e361d1a1bfb68d1c6780390d3059
Goryntsev Iurii <iuriigor@cleandata.ru>
2026-05-19 00:40:40 +0300

    После 1.2

---
+++ mcpulsor-server/src/main/java/ru/zgys/spring_ai_course_mcp/McpulsorServerApplication.java
@@ -0,0 +1,13 @@
+package ru.zgys.spring_ai_course_mcp;
+
+import lombok.SneakyThrows;
+
+public class McpulsorServerApplication {
+
+    @SneakyThrows
+    static void main() {
+        System.out.println("Server application started");
+
+
+    }
+}
